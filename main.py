511c5a14a135f6df422a38cd53810141790ce26e
VanosMC <132741302+VanosMC@users.noreply.github.com>
2023-08-23 22:20:55 +0200

    Probe Verkehrsampel

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-# mein Projekt
-
-from microbit import pin0, pin1, pin2, pin3, sleep
-
-#Alias vergeben
-s = pin0.read_analog()
+from microbit import pin1, pin2, pin5, sleep
 
 while True:
-    print(s)
-
-    if s >= 6000:
-        pin3.write_digital(1)
-        
-    elif s >= 4000 and s < 6000:
-        pin2.write_digital(1)
-
-    else:
-        pin1.write_digital(1)
+    pin1.write_digital(1)
+    sleep(2000)
     pin1.write_digital(0)
-    pin2.write_digital(0)
-    pin3.write_digital(0)
-    sleep(1000)
+    for x in range(3):
+        pin2.write_digital(1)
+        sleep(500)
+        pin2.write_digital(0)
+        sleep(500)
+    pin5.write_digital(1)
+    sleep(2000)
+    pin5.write_digital(0)
