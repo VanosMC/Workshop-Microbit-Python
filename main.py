6e91d2e8fdbb2dffa55a7b2f8d21a68bb100273b
VanosMC <132741302+VanosMC@users.noreply.github.com>
2023-08-24 22:53:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from microbit import pin1, pin2, pin5, sleep
+from microbit import pin0
+	
+#Alias vergeben
+s = pin0.read_analog()
 
 while True:
-    pin1.write_digital(1)
-    sleep(2000)
-    pin1.write_digital(0)
-    for x in range(3):
-        pin2.write_digital(1)
-        sleep(500)
-        pin2.write_digital(0)
-        sleep(500)
-    pin5.write_digital(1)
-    sleep(2000)
-    pin5.write_digital(0)
+    print("Lautstärke:",s)
